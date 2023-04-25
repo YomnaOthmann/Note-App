84bd9211a72f3037af66d18e093c6cd6f9b19316
YomnaOthmann <yomnaothmann@gmail.com>
2023-04-25 02:47:30 +0200

    added hive db to pubspec

--- lib/view/screens/edit_note_screen.dart
+++ lib/view/screens/edit_note_screen.dart
@@ -8,7 +8,7 @@ class EditnoteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
         body: EditNoteBody(),
       ),
