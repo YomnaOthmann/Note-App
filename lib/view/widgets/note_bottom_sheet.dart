d353cafa2746ba51bbe206988ab8924c167da52a
YomnaOthmann <yomnaothmann@gmail.com>
2023-04-27 21:51:04 +0200

    added blloc dependency

--- lib/view/widgets/note_bottom_sheet.dart
+++ lib/view/widgets/note_bottom_sheet.dart
@@ -59,9 +59,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 if (formKey.currentState!.validate()) {
                   formKey.currentState!.save();
                 } else {
-                  setState(() {
-                    autovalidateMode = AutovalidateMode.always;
-                  });
+                  autovalidateMode = AutovalidateMode.always;
+                  setState(() {});
                 }
               },
             )
